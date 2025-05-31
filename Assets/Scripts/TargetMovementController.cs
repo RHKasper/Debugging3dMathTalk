96b7e52d2c8a4e85be92d16aa943cfc93c8c1733
Robert Kasper <RobertHKasper@gmail.com>
2025-05-30 23:08:07 -0400

    adds target moving back and forth

---
+++ Assets/Scripts/TargetMovementController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class TargetMovementController : MonoBehaviour
+{
+    [SerializeField] private Transform waypoint;
+    [SerializeField] private float moveDuration = 5f;
+    [SerializeField] private float endPauseDuration = 2.5f;
+
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+    
+    private float _timeElapsed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _startPosition = transform.position;
+        _endPosition = waypoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(_startPosition, _endPosition, Mathf.Clamp01(_timeElapsed / moveDuration));
+
+        if (_timeElapsed >= moveDuration + endPauseDuration)
+        {
+            _timeElapsed = 0;
+            _endPosition = _startPosition;
+            _startPosition = transform.position;
+        }
+        
+        _timeElapsed += Time.deltaTime;
+    }
+}
